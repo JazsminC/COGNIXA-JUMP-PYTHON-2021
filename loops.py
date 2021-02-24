d6f84eb1e93d523b76d7b83f907f274f6145c25f
JazzyC16 <coronado.jazsmin@yahoo.com>
2021-02-24 15:34:36 -0600

    added some mor code to loops.py

---
+++ loops.py
@@ -0,0 +1,29 @@
+#variables
+#list_of_number = [0, 1, 2, 'blue', 4, 'green']  #list of numbers and strings
+
+# for loops
+#for elem in list_of_number:
+#    print(elem)
+
+#Another for loop example
+#for elem in list_of_number: #Goes through each element in the list
+#    if type(elem) == str:   #checks if there is a string element
+#        continue            # tells the loop to continue on to the next element
+#    print(elem)            #prints the elements in the loops that are not strings
+
+#range loop
+#for i in range(5):
+#    print(i)
+
+#while loop
+i = 0
+#while i < 10:
+ #   print(i)
+ #   i+=1
+
+#i = 0
+#while True:  #loops forever
+#    print(i)  #prints i
+#    i = i + 1  # incremets it
+#    if i == 11: #stops once i reaches 10
+#        break  #break the for loop
