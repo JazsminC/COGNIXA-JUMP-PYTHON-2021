e16af29a41d1a14b31c0f8af85d92997f4b325fb
JazzyC16 <coronado.jazsmin@yahoo.com>
2021-02-24 17:08:43 -0600

    created the files Q1.py,Q2.py, More_question.py

---
+++ More_Questions.py
@@ -0,0 +1,40 @@
+# Given an integer, n, perform the following condition actions
+# if n is odd, print weird
+# if n is even:
+#           * and in the inclusive range of 2 to 5
+#           * and in the inclusive range of 6 to 20
+#           * and greater than 20
+# Then print Not Werid
+
+
+def weird_or_Not(numbers):
+    for num in numbers:
+        if (num % 2) != 0: # if number is even
+            print("Weird")
+        else:
+            if num >= 2 and num <= 5:
+                print("Not weird")
+            elif num >= 6 and num <=20:
+                print("Weird")
+            elif num > 20:
+                print("Not weird")
+
+list_of_num = [1, 2, 3, 4, 8, 11, 18, 20]
+
+weird_or_Not(list_of_num)
+
+# 1.
+# Write a function that takes an array of integers and places the zeroes in the
+# center of the array. All non-zero integers should keep their relative position. 
+# For the purpose of this question, center is defined as floor(length of array / 2).
+
+# print all fibonnaci numbers less than 1000
+# Hint: when you don't know many times you do something, use a while loop
+# Hint: use a temp variable
+
+
+# 3.(This is super hard)
+# center_zeros([1, 1, 3, 0, 6, 0]) -> [1, 1, 0, 0, 3, 6]
+# center_zeros([0, 3, 1]) == [3, 0, 1]
+# center_zeros([1, 1, 3, 0]) == [1, 1, 0, 3]
+# center_zeros([1, 1, 3, 0, 6, 0]) == [1, 1, 0, 0, 3, 6]
---
+++ Q1_warmup.py
@@ -0,0 +1,20 @@
+# Use a for loop to print out the numbers 1 to 7
+
+#for i in range(8): 
+#    print(i)
+#****************************************************************
+
+# Use a while to print out the numbers 1 to 7
+#easy way to use while loop
+i = 0
+while i <= 7:
+    print(i)
+    i = i + 1
+
+#Another way to use while loop
+#i = 0
+#while True:
+#    print(i)
+#    i = i + 1
+#    if i == 8:
+#        break
