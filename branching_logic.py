4147151c725e4bf5f40ebe93486827765291917c
JazzyC16 <coronado.jazsmin@yahoo.com>
2021-02-24 15:04:35 -0600

    added code to branching_logic

---
+++ branching_logic.py
@@ -0,0 +1,15 @@
+like_python = False # change to get different results
+like_netflix = True  #change to get different results
+
+if like_python:
+    print("I'm a tautology")
+#elif will only print if what is above it is false
+elif like_netflix:
+    print("I will relax now")
+elif True:
+    print("second elif statement")
+else:
+    print("If everything is false above me, I will print this statement")
+
+
+
