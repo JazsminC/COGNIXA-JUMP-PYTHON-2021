94c5198314e3a9b5723abb78983fcb6d497d8ba2
JazzyC16 <coronado.jazsmin@yahoo.com>
2021-02-25 11:26:15 -0600

    added another warm up code questions

--- Q1_warmup.py
+++ Q1_warmup.py
@@ -1,20 +1,42 @@
 # Use a for loop to print out the numbers 1 to 7
 
-#for i in range(8): 
+#for i in range(1, 8): 
 #    print(i)
+
 #****************************************************************
 
 # Use a while to print out the numbers 1 to 7
 #easy way to use while loop
-i = 0
-while i <= 7:
-    print(i)
-    i = i + 1
+#j = 1
+#while j <= 7:
+#    print(i)
+#    j = j + 1
 
+#******************************************************************
 #Another way to use while loop
+
 #i = 0
 #while True:
 #    print(i)
 #    i = i + 1
 #    if i == 8:
 #        break
+
+# Given the participants' score sheet for your University Sport day,
+# you are required to find the runner-up score. You are given n scores.
+# Store them them in a list and find the score of the runner up
+#runner_up_score = [2, 3, 6, 6, 5]
+
+def second_place(runner_up_score):
+    # sort the list
+    runner_up_score.sort(reverse=True)
+
+    first_place_score = runner_up_score[0]
+
+    for elem in runner_up_score:
+        if elem < first_place_score:
+            return_string = "Here is second place with a score of {}!".format(elem)
+            return return_string
+
+
+print(second_place(runner_up_score))
